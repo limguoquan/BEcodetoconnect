cd67d87be12379dab3adb9bf4678a09b01078df9
limguoquan <49220059+limguoquan@users.noreply.github.com>
2022-07-09 19:13:47 +0800

    Initial commit

---
+++ src/test/java/com/example/BEcodetoconnect/BEcodetoconnectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.BEcodetoconnect;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BEcodetoconnectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
